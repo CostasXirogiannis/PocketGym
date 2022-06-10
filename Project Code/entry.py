2b77115cef288b63c96e81cc4ca4d2eb71a9fb30
tzavk <65625667+tzavk@users.noreply.github.com>
2022-06-10 12:26:58 +0300

    Add files via upload

--- Project Code/entry.py
+++ Project Code/entry.py
@@ -24,7 +24,145 @@ KV = '''
             halign: 'center'
             font_size: '34sp'
             size_hint: 1, 0.1
+ 
+        BoxLayout:
+            orientation: 'vertical'
+            size_hint: 1, 0.9
+            padding: (5,5,5,5)
 
+            canvas:
+                Color:
+                    rgba: 0.95,0.95,0.95, 0.5
+                Rectangle:
+                    pos: self.pos
+                    size: self.size
+
+            BoxLayout:
+                spacing: 10
+
+                canvas.before:
+                    Color:
+                        rgba: 0.85,0.85,0.85, 1
+                    Line:
+                        points: self.x, self.y, self.x+self.width,self.y
+
+                orientation: 'horizontal'
+                MDIconButton:
+                    icon: 'account-outline'
+                    md_bg_color: (0,0,0,0.1)
+                    user_font_size: '20sp'
+                    pos_hint: {'center_x': 0.05, 'center_y': 0.5}
+
+                Label:
+                    text: '[ref=koko]Προφίλ[/ref]'
+                    markup: True
+                    color: 0,0,0, 0.7
+                    text_size: self.size
+                    size_hint: (1, 1)
+                    halign: "left"
+                    valign: "middle"
+                    on_ref_press: root.on_profile()
+
+            BoxLayout:
+                spacing: 10
+
+                canvas.before:
+                    Color:
+                        rgba: 0.85,0.85,0.85, 1
+                    Line:
+                        points: self.x, self.y, self.x+self.width,self.y
+
+                orientation: 'horizontal'
+                MDIconButton:
+                    icon: 'plus-thick'
+                    md_bg_color: (0,0,0,0.1)
+                    user_font_size: '20sp'
+                    pos_hint: {'center_x': 0.05, 'center_y': 0.5}
+
+                Label:
+                    text: 'Νέα δραστηριότητα'
+                    color: 0,0,0, 0.7
+                    text_size: self.size
+                    size_hint: (1, 1)
+                    halign: "left"
+                    valign: "middle"
+
+            BoxLayout:
+                spacing: 10
+
+                canvas.before:
+                    Color:
+                        rgba: 0.85,0.85,0.85, 1
+                    Line:
+                        points: self.x, self.y, self.x+self.width,self.y
+
+
+                orientation: 'horizontal'
+                MDIconButton:
+                    icon: 'history'
+                    md_bg_color: (0,0,0,0.1)
+                    user_font_size: '20sp'
+                    pos_hint: {'center_x': 0.05, 'center_y': 0.5}
+
+                Label:
+                    text: '[ref=history]Ιστορικό[/ref]'
+                    markup: True
+                    color: 0,0,0, 0.7
+                    text_size: self.size
+                    size_hint: (1, 1)
+                    halign: "left"
+                    valign: "middle"
+                    on_ref_press: root.on_history()
+
+            BoxLayout:
+                spacing: 10
+
+                canvas.before:
+                    Color:
+                        rgba: 0.85,0.85,0.85, 1
+                    Line:
+                        points: self.x, self.y, self.x+self.width,self.y
+
+
+                orientation: 'horizontal'
+                MDIconButton:
+                    icon: 'star'
+                    md_bg_color: (0,0,0,0.1)
+                    user_font_size: '20sp'
+                    pos_hint: {'center_x': 0.05, 'center_y': 0.5}
+
+                Label:
+                    text: 'Premium'
+                    color: 0,0,0, 0.7
+                    text_size: self.size
+                    size_hint: (1, 1)
+                    halign: "left"
+                    valign: "middle"
+
+            BoxLayout:
+                spacing: 10
+
+                canvas.before:
+                    Color:
+                        rgba: 0.85,0.85,0.85, 1
+                    Line:
+                        points: self.x, self.y, self.x+self.width,self.y
+
+                orientation: 'horizontal'
+                MDIconButton:
+                    icon: 'cog-outline'
+                    md_bg_color: (0,0,0,0.1)
+                    user_font_size: '20sp'
+                    pos_hint: {'center_x': 0.05, 'center_y': 0.5}
+
+                Label:
+                    text: 'Ρυθμίσεις'
+                    markup: True
+                    color: 0,0,0, 0.7
+                    text_size: self.size
+                    size_hint: (1, 1)
+                    halign: "left"
+                    valign: "middle"
        
         PGScreenSelector:
 '''
