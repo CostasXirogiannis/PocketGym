963d5b16fab16671981fd0c1ffd0159c771d3f9b
apostolisg97 <80760628+apostolisg97@users.noreply.github.com>
2022-06-12 17:38:41 +0300

    Update create_prof.py

--- Project Code/create_prof.py
+++ Project Code/create_prof.py
@@ -1,6 +1,6 @@
 from kivy.lang import Builder
 from kivy.uix.screenmanager import Screen
-from kivy.uix.textinput import TextInput
+from kivy.uix.textinput import TextInpu
 
 
 # This is the profile creation screen, which user will see after opening the app
