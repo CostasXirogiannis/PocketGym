23a0ad45fd4c292299013576a0d9f3550c6db1c6
apostolisg97 <80760628+apostolisg97@users.noreply.github.com>
2022-06-12 07:56:27 +0300

    Update welcome.py

--- Project Code/welcome.py
+++ Project Code/welcome.py
@@ -20,3 +20,15 @@ KV = '''
             Rectangle:
                 pos: self.pos
                 size: self.size
+        Label:
+            pos_hint: {'center_x': 0.5, 'center_y': 0.5}
+            text: 'PocketGym'
+            color: 0.15,0.12,0.54, 2
+            halign: 'center'
+            font_size: '34sp'
+            size_hint: 1, 0.1
+        
+        BoxLayout:
+            orientation: 'vertical'
+            size_hint: 1, 0.9
+            padding: (5,5,5,5)
