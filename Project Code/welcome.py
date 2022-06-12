e203fe114608a339223e279900318688af02da9d
apostolisg97 <80760628+apostolisg97@users.noreply.github.com>
2022-06-12 08:24:49 +0300

    Update welcome.py

--- Project Code/welcome.py
+++ Project Code/welcome.py
@@ -32,3 +32,18 @@ KV = '''
             orientation: 'vertical'
             size_hint: 1, 0.9
             padding: (5,5,5,5)
+        canvas:
+                Color:
+                    rgba: 0.95,0.95,0.95, 0.5
+                Rectangle:
+                    pos: self.pos
+                    size: self.size
+
+        Label:
+                pos_hint: {'center_x': 0.5, 'center_y': 0.1}
+                text: 'Welcome!'
+                color: 0.15,0.12,0.54, 1
+                halign: 'center'
+                text_size: self.size
+                font_size: '210sp'
+                size_hint: 1, 0.2
