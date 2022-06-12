d3278745e60830e5242ab7ebd4a0bdaa9fe4c0d7
apostolisg97 <80760628+apostolisg97@users.noreply.github.com>
2022-06-12 13:19:23 +0300

    Create signin.py

---
+++ Project Code/signin.py
@@ -0,0 +1,8 @@
+from kivy.lang import Builder
+from kivy.uix.screenmanager import Screen
+from kivy.uix.textinput import TextInput
+
+
+# This is the profile creation screen, which user will see after opening the app
+
+KV = '''
