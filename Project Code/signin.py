ffb1f160e0154678733bb49f8e6aa4b149513291
apostolisg97 <80760628+apostolisg97@users.noreply.github.com>
2022-06-12 13:24:13 +0300

    Update signin.py

--- Project Code/signin.py
+++ Project Code/signin.py
@@ -6,3 +6,11 @@ from kivy.uix.textinput import TextInput
 # This is the profile creation screen, which user will see after opening the app
 
 KV = '''
+#: import Window kivy.core.window.Window
+
+<Create_prof>:
+    GridLayout:
+        cols: 1
+        size: Window.size
+        padding: (0,0,0,0)
+        spacing: 1
