4cf3a6962660aaa444e2cb4a07f59c59a8c6a12a
apostolisg97 <80760628+apostolisg97@users.noreply.github.com>
2022-06-12 07:26:41 +0300

    Create welcome.py

---
+++ Project Code/welcome.py
@@ -0,0 +1,9 @@
+from kivy.lang import Builder
+from kivy.uix.screenmanager import Screen
+from kivy.uix.textinput import TextInput
+
+
+# This is the welcome screen, which user will see when opening the app
+
+KV = '''
+#: import Window kivy.core.window.Window
